c8963e7605beed251d67e4d506fb0e65b57f5f63
Ikaro <ikaroassis08@gmail.com>
2023-06-06 19:11:24 -0300

    Instagram

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Footer from './components/Footer.js';
+import Main from './components/main.js'
+function App() {
+  return (
+    <div className="App">
+    <Main/>
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
